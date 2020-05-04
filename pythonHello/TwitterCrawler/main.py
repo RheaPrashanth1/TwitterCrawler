770787534a9e13ed74707118015426fd6b707850
RheaPrashanth1 <43652961+RheaPrashanth1@users.noreply.github.com>
2020-05-03 22:12:54 -0700

    Update main.py

--- pythonHello/TwitterCrawler/main.py
+++ pythonHello/TwitterCrawler/main.py
@@ -1,47 +1,48 @@
 #Import the necessary methods from tweepy library
-import tweepy
-import json
-import sys
-import os
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
+import json
 
+tweetCount = 0
 # Enter Twitter API Keys
 access_token = "3028914487-zriqd4Qo4DUjj1IJZbAPC6McsaALwYiSMICvfms"
 access_token_secret = "u929aKlFsSRjT2eTAOhBWIj2hd4FYVJjeU5nTqkXwH2GV"
 consumer_key = "r9Zv2p5DWekyQnC1aXHD8k5ks"
 consumer_secret = "QITRb64e44ChymHmYFKRWSAjS43TbJmCA8fwa2G6AtEnnvNm7t"
-filecount = 0
-tweetLimit =  0
+OF = open("tweet.txt", "a")
+filenum = 0
 #outF = open("tweet.txt", "w")
 #outF = open("tweet.txt", "w")
 
 # Listener class inherits from StreamListener
 class Listener(StreamListener):
-      
-    def __init__(self, api=None):
-       # super(listener, self).__init__()
-        self.num_tweets = 0
+    
+    def on_data(self,data):
+        global tweetCount 
+        global OF
+        global filenum 
 
-    def on_connect(self):
-        print("Connection is established")
+        Dict = json.loads(data)  #JSON object to python DICT
+        Dict.update({"before" : 23}) #append to DICT
+        finalTweet = json.dumps(Dict) #convert DICT to JSON
+        tweetCount = tweetCount + 1
+        OF.write(finalTweet + "\n") #put JSON objects one at a time on newline
+        print(finalTweet)
 
-    def on_data(self, data):   
-      
-        decoded = json.loads(data) #turn JSON into python dict for each JSON object
-        id = decoded["created_at"]
-        outF = open("tweet.txt", "w")
+        if  OF.tell() >= 10*1024*1024:  #If file count > 10 MB open new text file
+            tweetCount = 0
+            filenum = filenum + 1
+            newfile = "tweet" + str(filenum) + ".txt"
+            OF = open(newfile, "a")
 
+        if  filenum == 200:  #If 2GB of total data, stop streaming
+            print("StreamListener no longer streaming tweets")
+            return False
 
-        print(id)
-     #   print(data)
-      
-       
-        
         return True
         
-
+    
     def on_error(self, tweetstatus):
         if tweetstatus == 420:
             print("Error occured, loser")
