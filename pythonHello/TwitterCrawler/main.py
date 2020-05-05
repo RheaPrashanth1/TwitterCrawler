eeb21e366a16a9ccc3f3d60df660f3a6c01e6571
RheaPrashanth1 <43652961+RheaPrashanth1@users.noreply.github.com>
2020-05-04 21:42:25 -0700

    Update main.py

--- pythonHello/TwitterCrawler/main.py
+++ pythonHello/TwitterCrawler/main.py
@@ -3,6 +3,11 @@ from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 import json
+import re
+from lxml.html import parse
+import urllib.request as urllib2
+import argparse
+
 
 tweetCount = 0
 # Enter Twitter API Keys
@@ -12,6 +17,7 @@ consumer_key = "r9Zv2p5DWekyQnC1aXHD8k5ks"
 consumer_secret = "QITRb64e44ChymHmYFKRWSAjS43TbJmCA8fwa2G6AtEnnvNm7t"
 OF = open("tweet.txt", "a")
 filenum = 0
+#title = 5
 #outF = open("tweet.txt", "w")
 #outF = open("tweet.txt", "w")
 
@@ -22,30 +28,76 @@ class Listener(StreamListener):
         global tweetCount 
         global OF
         global filenum 
+       # global title
+        appended = "ran"
+        
 
+      
         Dict = json.loads(data)  #JSON object to python DICT
-        Dict.update({"before" : 23}) #append to DICT
+        tweetURLS = Dict["entities"]["urls"]
+       # Dict.update({"before" : 23}) #append to DICT
         finalTweet = json.dumps(Dict) #convert DICT to JSON
         tweetCount = tweetCount + 1
-        OF.write(finalTweet + "\n") #put JSON objects one at a time on newline
-        print(finalTweet)
+       # OF.write(finalTweet + "\n") #put JSON objects one at a time on newline
+       # print(finalTweet)
+        title = None
 
-        if  OF.tell() >= 10*1024*1024:  #If file count > 10 MB open new text file
+#if OF.tell() >= 10*1024*1024: 
+        if OF.tell() >= 10*1024*1024:    #If file count > 10 MB open new text file
             tweetCount = 0
             filenum = filenum + 1
             newfile = "tweet" + str(filenum) + ".txt"
             OF = open(newfile, "a")
 
-        if  filenum == 200:  #If 2GB of total data, stop streaming
-            print("StreamListener no longer streaming tweets")
+        if filenum == 3:  #If 2GB of total data, stop streaming
+            print("StreamListener no longer streaming tweets - reached 2GB")
             return False
+        
+        if tweetURLS!= []:
+            encodedUrl = Dict['entities']['urls'][0]['expanded_url']
+            try:
+                openedURL = urllib2.urlopen(encodedUrl)
+                parsedPage = parse(openedURL)
+                title = parsedPage.find(".//title").text
+                
+            except:
+                print("error")
+				
+           # print(title)
+        #title = parsedPage.find(".//title").text  
+        if tweetURLS == []:
+            title = None
+            
 
+        if  title != None:
+           
+            title = re.sub('[^A-Za-z0-9]+', ' ', title)  
+            #print(title) 
+
+            Dict.update({"Title" : title}) #append to python dict
+           # print(Dict["Title"])
+            appended = json.dumps(Dict) #pythonDict to JSON
+            
+        if title == None:
+            Dict.update({"Title" : "None"})
+            appended = json.dumps(Dict)
+            
+       
+        OF.write(appended + "\n")
+        
+        #Check if it can be read back
+        app = json.loads(appended) #python dict
+        checkIfReadable = app["Title"]
+        print(checkIfReadable)
+        
+        
+        
         return True
         
     
     def on_error(self, tweetstatus):
         if tweetstatus == 420:
-            print("Error occured, loser")
+            print("Error occured!!!!")
         if tweetstatus == 401:
             print("Authentication error")
             return False
